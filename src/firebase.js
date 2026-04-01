4cac0c7a4d19026a644ea834b69412c77a1f17e5
restaurantlembouchure-dev <restaurant.lembouchure@gmail.com>
2026-04-01 13:27:39 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBQCHPgf-jAGg6JU3bJHHPZXjd9Ii6ME9w",
   authDomain: "embouchure-fdcce.firebaseapp.com",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "1086400525281",
   appId: "1:1086400525281:web:e61bc47ad06d6f738f58a0"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
