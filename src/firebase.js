f23541e5b90bbc3469b4e5bd76e7c40d3fc74ba0
restaurantlembouchure-dev <restaurant.lembouchure@gmail.com>
2026-04-01 12:45:10 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQCHPgf-jAGg6JU3bJHHPZXjd9Ii6ME9w",
+  authDomain: "embouchure-fdcce.firebaseapp.com",
+  projectId: "embouchure-fdcce",
+  storageBucket: "embouchure-fdcce.firebasestorage.app",
+  messagingSenderId: "1086400525281",
+  appId: "1:1086400525281:web:e61bc47ad06d6f738f58a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
